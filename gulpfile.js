831814b1b47eb3f98390eec939b9c4e78e60ceb4
Jeff Posnick <jeffy@google.com>
2016-09-10 14:28:50 -0400

    Task reordering

--- gulpfile.js
+++ gulpfile.js
@@ -16,20 +16,14 @@ const swPrecache = require('sw-precache');
 
 const BUILD_DIR = 'build';
 
-gulp.task('localhost', callback => {
-  spawn('node_modules/.bin/http-server', [
-    BUILD_DIR,
-    '-p', '8000',
-    '-a', '127.0.0.1',
-    '-c', '-1'
-  ]).on('exit', callback);
+gulp.task('clean', () => {
+  return del(BUILD_DIR);
 });
 
 gulp.task('jekyll:build', callback => {
   spawn('jekyll', ['build', '--destination', BUILD_DIR], {stdio: 'inherit'})
     .on('exit', callback);
 });
-
 
 gulp.task('sass', () => {
   return gulp.src('_sass/main.scss')
@@ -50,58 +44,6 @@ gulp.task('site-metadata', callback => {
   }).sort((a, b) => a.date < b.date);
 
   fs.writeFile(`${BUILD_DIR}/posts.json`, JSON.stringify(posts), callback);
-});
-
-const bundler = browserify({
-  entries: 'src/jekyll-behavior-import.js',
-  transform: ['babelify']
-});
-
-gulp.task('browserify', () => {
-  return bundler.bundle()
-    .on('error', console.error)
-    .pipe(source('jekyll-behavior-import.js'))
-    .pipe(buffer())
-    .pipe(gulp.dest(BUILD_DIR));
-});
-
-gulp.task('service-worker', ['browserify'], () => {
-  return swPrecache.write(`${BUILD_DIR}/service-worker.js`, {
-    stripPrefix: `${BUILD_DIR}/`,
-    staticFileGlobs: [
-      '_config.yml',
-      'posts.json',
-      'manifest.json',
-      '_layouts/**/*.html',
-      '_includes/**/*.html',
-      '_posts/**/*.markdown'
-    ].map(glob => `${BUILD_DIR}/${glob}`),
-    importScripts: ['jekyll-behavior-import.js'],
-    runtimeCaching: [{
-      urlPattern: /\/assets\/images\//,
-      handler: 'cacheFirst',
-      options: {
-        cache: {
-          maxEntries: 20,
-          name: 'images'
-        }
-      }
-    }, {
-      urlPattern: /\/css\/.*\.css$/,
-      handler: 'fastest'
-    }, {
-      urlPattern: /\/\/fonts\./,
-      handler: 'fastest'
-    }]
-  });
-});
-
-gulp.task('watch', ['browserify'], () => {
-  return gulp.watch('src/**/*.js', ['browserify']);
-});
-
-gulp.task('clean', () => {
-  return del(BUILD_DIR);
 });
 
 gulp.task('copy-raw-files', () => {
@@ -135,6 +77,54 @@ gulp.task('minify:css', () => {
   });
 });
 
+const bundler = browserify({
+  entries: 'src/jekyll-behavior-import.js',
+  transform: ['babelify']
+});
+
+gulp.task('bundle', () => {
+  return bundler.bundle()
+    .on('error', console.error)
+    .pipe(source('jekyll-behavior-import.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest(BUILD_DIR));
+});
+
+gulp.task('bundle:watch', ['bundle'], () => {
+  return gulp.watch('src/**/*.js', ['bundle']);
+});
+
+gulp.task('service-worker', ['bundle'], () => {
+  return swPrecache.write(`${BUILD_DIR}/service-worker.js`, {
+    stripPrefix: `${BUILD_DIR}/`,
+    staticFileGlobs: [
+      '_config.yml',
+      'posts.json',
+      'manifest.json',
+      '_layouts/**/*.html',
+      '_includes/**/*.html',
+      '_posts/**/*.markdown'
+    ].map(glob => `${BUILD_DIR}/${glob}`),
+    importScripts: ['jekyll-behavior-import.js'],
+    runtimeCaching: [{
+      urlPattern: /\/assets\/images\//,
+      handler: 'cacheFirst',
+      options: {
+        cache: {
+          maxEntries: 20,
+          name: 'images'
+        }
+      }
+    }, {
+      urlPattern: /\/css\/.*\.css$/,
+      handler: 'fastest'
+    }, {
+      urlPattern: /\/\/fonts\./,
+      handler: 'fastest'
+    }]
+  });
+});
+
 gulp.task('build', callback => {
   runSequence(
     'clean',
@@ -152,4 +142,13 @@ gulp.task('deploy', ['build'], callback => {
     message: 'Automated build.',
     dotfiles: true
   }, callback);
+});
+
+gulp.task('localhost', callback => {
+  spawn('node_modules/.bin/http-server', [
+    BUILD_DIR,
+    '-p', '8000',
+    '-a', '127.0.0.1',
+    '-c', '-1'
+  ]).on('exit', callback);
 });
