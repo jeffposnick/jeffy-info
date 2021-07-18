ab614db64ec455e7aa3d8284825ade8e72c3d9d0
Jeff Posnick <jeffy@google.com>
2021-07-18 09:51:42 -0400

    Twitter meta

--- static/set-metadata.js
+++ static/set-metadata.js
@@ -2,5 +2,10 @@ function setMetadata(site, page) {
   const title = page.title || site.title;
   if (title) {
     document.title = title;
+
+    const titleEl = document.createElement('meta');
+    titleEl.setAttribute('name', 'twitter:title');
+    titleEl.setAttribute('content', title);
+    document.head.appendChild(titleEl);
   }
 }
