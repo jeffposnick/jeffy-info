778c7e33814d3ccb06b59a56f18b7565348a984b
Jeff Posnick <jeffy@google.com>
2021-07-19 09:28:29 -0400

    Remove Twitter meta tags

--- src/window/page.ts
+++ src/window/page.ts
@@ -2,10 +2,5 @@ export function setMetadata(site: Record<string, any>, page: Record<string, any>
   const title = page.title || site.title;
   if (title) {
     document.title = title;
-
-    const titleEl = document.createElement('meta');
-    titleEl.setAttribute('name', 'twitter:title');
-    titleEl.setAttribute('content', title);
-    document.head.appendChild(titleEl);
   }
 }
