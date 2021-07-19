71a94dde74b746c41e8a2f665a04c14f4b17fe56
Jeff Posnick <jeffy@google.com>
2021-07-19 13:23:28 -0400

    Hardcode a location "polyfill"

--- src/service-worker/cf-sw.ts
+++ src/service-worker/cf-sw.ts
@@ -1,9 +1,23 @@
 /// <reference lib="webworker"/>
 
-import '@worker-tools/location-polyfill';
 import { getAssetFromKV, mapRequestToAsset, NotFoundError } from '@cloudflare/kv-asset-handler';
 
 import { registerRoutes, StaticLoader } from './shared/common';
+
+// self.location is referenced a few places in Workbox, but it's not required
+// to be set to anything meaningful for this use case.
+if (!('location' in self)) {
+  Object.defineProperty(self, 'location', {
+    configurable: false,
+    enumerable: true,
+    writable: false,
+    value: {
+      hostname: 'example.com',
+      href: 'https://example.com/',
+      origin: 'https://example.com',
+    },
+  });
+}
 
 const loadStatic: StaticLoader = async (event, urlOverride) => {
   const options = urlOverride
