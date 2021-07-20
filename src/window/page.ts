3131f6d3e9ba4b0c482e2a3480ae72c564cb86f3
Jeff Posnick <jeffy@google.com>
2021-07-20 12:46:58 -0400

    Add JSON-LD tag

--- src/window/page.ts
+++ src/window/page.ts
@@ -3,7 +3,21 @@ export function setMetadata(
   page: Record<string, any>,
 ): void {
   const title = page.title || site.title;
+
   if (title) {
     document.title = title;
+
+    // See https://developers.google.com/search/docs/data-types/article#non-amp
+    const schemaData = {
+      '@context': 'https://schema.org',
+      '@type': 'BlogPosting',
+      datePublished: page.date,
+      headline: title,
+      image: [site.logo],
+    };
+    const scriptEl = document.createElement('script');
+    scriptEl.setAttribute('type', 'application/ld+json');
+    scriptEl.innerText = JSON.stringify(schemaData);
+    document.head.appendChild(scriptEl);
   }
 }
